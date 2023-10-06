323240e64d263524e6fba9dc6792a96325ff561c
danny6543211 <danny6543211@gmai.com>
2023-10-06 21:46:16 +0800

    add list.hpp

--- list.hpp
+++ list.hpp
@@ -11,17 +11,18 @@ namespace mystl {
 template<class T>
 struct __list_node {
     typedef __list_node<T>* node_pointer;
+    T data;
     node_pointer prev;
     node_pointer next;
-    T data;
+    __list_node() : data(), prev(), next() {}
+    __list_node(T data, node_pointer prev, node_pointer next) 
+    : data(data), prev(prev), next(next) {}
 };
 
 template<class T>
-struct __list_iterator {
+struct __list_iterator : public mystl::iterator<mystl::bidirectional_iterator_tag, T> {
     typedef __list_iterator<T> iterator;
-
     // 双向访问迭代器
-    typedef mystl::bidirectional_iterator_tag   iterator_category;
     typedef T                                   value_type;
     typedef T*                                  pointer;
     typedef T&                                  reference;
@@ -72,6 +73,7 @@ public:
     typedef T                   value_type;
     typedef T*                  pointer;
     typedef T&                  reference;
+    
     typedef __list_iterator<T>  iterator;
     typedef __list_node<T>      list_node;
     
@@ -80,22 +82,68 @@ protected:
     list_node* head;
 
 public:
-    list() {}
-    list(const std::initializer_list<T>& x) {}
-    list(const list<T>& x) {}
-    list(const iterator begin, const iterator end) {}
-    list(const size_t n, const T& x) {}
+    static void test() {
+        list<int> a(4, 1);
+        for (auto i = a.begin(); i != a.end(); i++) {
+            std::cout << *i;
+        }
+    
+    }
 
-    iterator begin() {}
-    iterator end() {}
-    iterator rbegin() {}
-    iterator rend() {}
 
-    size_t size() {}
-    bool empty() {}
+    list() {
+        // 头结点不存数据
+        head = new list_node;
+        head->next = head;
+        head->prev = head;
+    }    
 
-    reference front() {}
-    reference back() {}
+    list(const std::initializer_list<T>& x) : list() {
+        list_node *p = head;
+        for (auto i = x.begin(); i != x.end(); i++) {
+            p->next = new list_node(*i, p, nullptr);
+            p = p->next;
+        }
+        p->next = head;
+        head->prev = p;
+    }
+
+    list(const list<T>& x) : list(x.begin(), x.end()) {}
+ 
+    list(const iterator begin, const iterator end) : list() {
+        list_node *p = head;
+        for (iterator i = begin;i != end; i++) {
+            p->next = new list_node(*i, p, nullptr);
+            p = p->next;
+        }
+        // 头尾接起来
+        p->next = head;
+        head->prev = p;
+    }
+
+    list(const size_t n, const T& x) : list() {
+        list_node *p = head;
+        for (size_t i = 0; i < n; i++) {
+            p->next = new list_node(x, p, nullptr);
+            p = p->next;
+        }
+        p->next = head;
+        head->prev = p;
+    }
+
+    iterator begin() { return head->next; }
+    iterator end() { return head; }
+    iterator rbegin() { return head->prev; }
+    iterator rend() { return head; }
+
+    size_t size() {
+        return mystl::distance(begin(), end());
+    }
+
+    bool empty() { return size() == 0; }
+
+    reference front() const { return head->next->data; }
+    reference back() const { return head->prev->data; }
 
     void push_back() {}
     void pop_back() {}
@@ -104,15 +152,12 @@ public:
     void pop_front() {}
 
     iterator insert() {}
-    clear() {}
-    erase() {}
-    remove() {} 
+    void clear() {}
+    void erase() {}
+    void remove() {} 
+
 };
 
-void test() {
-    
-
-}
 
 
 } /* end of mystl */
